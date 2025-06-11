f767c197d6a1974548f3c33776934dc22b8e8771
fatima alzahraa almaz <almazfatima9@gmail.com>
2025-06-11 12:15:58 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,58 @@
+let ch=document.getElementById('ch');
+let Block=document.getElementById('block');
+let dog=document.getElementById('dog');
+let box1=document.getElementById('box1');
+let h1=document.getElementById('score');
+let blockimg=document.getElementById('blockimg');
+let click=new Audio('jump.mp3');
+let lose=new Audio('gameover.mp3');
+ 
+var a=0 ; let stopincrease;
+
+ 
+ function hundleclick(){
+  click.play()
+  if(ch.classList!='animate')
+  ch.classList.add('animate');
+  setTimeout(function(){
+    ch.classList.remove('animate')
+  },1000)
+ 
+}
+function isDead()
+{
+  let chtop=parseInt(window.getComputedStyle(ch).getPropertyValue('top'));
+  let leftb=parseInt(window.getComputedStyle(Block).getPropertyValue('left'));
+  if(chtop>180 && leftb>0 && leftb<=175)
+  {   lose.play();
+    alert('Game Over XD !!');
+    
+    
+      
+     
+    
+    
+    Block.style.animation='none';
+    dog.style.animation='none';
+    box1.style.animation='none';
+    blockimg.style.animation='none';
+    
+    clearInterval(stopincrease)
+     h1.innerHTML='score :'+a +' Refresh the page to try again :)';
+
+  }
+   
+   
+}
+setInterval(isDead,100);
+ 
+ 
+function increase()
+{
+a++;
+h1.innerHTML='scores :'+a;
+console.log(a);
+}
+ 
+stopincrease=setInterval( increase,100);
+ 
